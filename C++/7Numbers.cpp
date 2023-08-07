27b5a5b40ee4d292248adc48aa0b0b224e8a8854
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-07 15:36:05 +0530

    First commit

--- C++/7Numbers.cpp
+++ C++/7Numbers.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout<<40\n;
-    cout<<5+7\n;
-    cout<<5/7\n;
-    cout<<5*7\n;
+    cout<<40<<endl;
+    cout<<5+7<<endl;
+    cout<<5/7<<endl;
+    cout<<5*7<<endl;
     cout<<10%3;
-    
+
     return 0;
 }
