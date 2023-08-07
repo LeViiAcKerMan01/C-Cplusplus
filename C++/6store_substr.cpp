09efb0176758a5a547ab8d325fbe9ef014155858
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-07 15:24:08 +0530

    First commit

--- C++/6store_substr.cpp
+++ C++/6store_substr.cpp
@@ -1,2 +1,11 @@
 #include<iostream>
 using namespace std;
+
+int main()
+{
+    string phrase="Giraffe Academy";
+    string phraseSub;
+    phraseSub=phrase.substr(8,3);
+    cout<<phraseSub;
+    return 0;
+}
