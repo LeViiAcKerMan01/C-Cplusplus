fc5db62dadf1456262126fa79a25fb211a01e2fa
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-07 15:07:48 +0530

    First commit

---
+++ C++/3variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;  //  initialization
+    string characterName="John";  //  declaration + initialization
+    age=35;  //  declaration
+
+    return 0;
+
+}
