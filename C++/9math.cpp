1be20154e2bdb075b032e438a255d440cecf37eb
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-07 15:52:17 +0530

    First commit

---
+++ C++/9math.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<pow(3, 3)<<endl;
+    cout<<sqrt(36)<<endl;
+    cout<<round(4.3)<<endl;
+    cout<<ceil(4.1)<<endl;
+    cout<<floor(4.7)<<endl;
+    cout<<fmax(3,10)<<endl;
+    cout<<fmin(3,10)<<endl;
+    return 0;
+}
