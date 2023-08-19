6fa3c2b3dc05be9dc03ba615d65b2b8479106604
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 15:43:21 +0530

    First commit

---
+++ C++/114functions.cpp
@@ -0,0 +1,28 @@
+// Program-1
+/*#include<iostream>
+using namespace std;
+
+void sayHi(){
+    cout<<"Hello user";
+
+}
+
+int main()
+{
+    sayHi();
+    return 0;
+
+}*/
+
+
+#include<iostream>
+using namespace std;
+
+void sayHi(string name){
+    cout<<"Hello "<<name;
+}
+int main()
+{
+    sayHi("Mike");
+    return 0;
+}
