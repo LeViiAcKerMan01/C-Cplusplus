b3ad9231b0b96f27381448943c6ab22994fe0afa
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 23:18:50 +0530

    First commit

---
+++ C++/126_2Darray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int numberGrid[3][2]={
+                         {1, 2},
+                         {3, 4},
+                         {5, 6}
+                         };
+    cout<<numberGrid[0][1]<<endl;
+    cout<<numberGrid[2][0]<<endl;
+
+        return 0;
+}
+// simple 2D array elements obtained as output.
