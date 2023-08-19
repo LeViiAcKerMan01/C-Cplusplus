a37b65bf24c43de3f48bbcbba3e868895d34af2d
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 15:51:08 +0530

    First commit

--- C++/114functions.cpp
+++ C++/114functions.cpp
@@ -14,7 +14,7 @@ int main()
 
 }*/
 
-
+/*
 #include<iostream>
 using namespace std;
 
@@ -25,4 +25,26 @@ int main()
 {
     sayHi("Mike");
     return 0;
+}*/
+
+
+#include<iostream>
+using namespace std;
+
+void sayHi(string name, int age){
+    cout<<"Hello "<<name<<" You are "<<age;
+
+}
+int main()
+{
+    sayHi("Priyanshu", 19);
+    cout<<"\n";
+    sayHi("Levi", 20);
+    cout<<"\n";
+    sayHi("Steve",20);
+    cout<<"\n";
+    sayHi("Mike",20);
+    
+    return 0;
+
 }
