40432e223e650903ce0298d501b6bc21765b739b
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 22:30:07 +0530

    First commit

---
+++ C++/120while_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int index=1;
+    while(index<=5){  // Condition for while loop
+        cout<<index<<endl;
+        index++;  // INCREMENT
+
+    }
+    return 0;
+}
