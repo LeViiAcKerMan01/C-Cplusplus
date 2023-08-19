dc0e41c4a86e8f25cc9505c30388609b44c63bcf
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 16:36:06 +0530

    First commit

---
+++ C++/116if_statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    bool isMale=true;
+    bool isTall=true;
+
+    if(isMale && isTall){
+        cout<<"You are a tall male";
+    }
+    else{
+        cout<<"You are not male";
+    }
+    return 0;
+}
