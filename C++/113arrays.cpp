70aff43e11614ff880d2f2c964da20890d32a4b6
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 14:32:42 +0530

    First commit

---
+++ C++/113arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int luckyNums[]={4, 8, 15, 16, 23, 42};
+    
+    cout<<luckyNums[0]<<endl;
+    cout<<luckyNums[1]<<endl;
+
+    return 0;
+}
