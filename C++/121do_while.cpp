e9520846f5fad15567378a4d0c3b5f0980ce11c9
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-19 22:58:42 +0530

    First commit

--- C++/121do_while.cpp
+++ C++/121do_while.cpp
@@ -12,3 +12,9 @@ int main()
     while(index<=5);
     return 0;
 }
+/* 
+do while loop :
+ 
+first executes the block of code and then look over the condition and if the condition holds true only then it will execute the block of code further else it stops!
+
+*/
