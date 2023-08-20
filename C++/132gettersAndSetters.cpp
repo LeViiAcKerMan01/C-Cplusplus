41be1aebfd6cec6edfcbd403b63ac6dc8515094d
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-20 20:56:47 +0530

    First commit

---
+++ C++/132gettersAndSetters.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Movie{
+    private:
+    string rating;
+    public:
+    string title;
+    string director;
+    Movie(string aTitle, string aDirector, string aRating){
+        title=aTitle;
+        director=aDirector;
+        setRating(aRating);
+    }
+    void setRating(string aRating){
+        if(aRating=="G" || aRating=="PG" || aRating=="PG-13" || aRating=="R" || aRating=="NR"){
+            rating=aRating;
+        }else{
+            rating="NR";
+        }
+    }
+    string getRating(){
+        return rating;
+    }
+};
+int main()
+{
+    Movie avengers("The Avengers", "Joss Whedon", "PG-13");
+
+    cout<<avengers.getRating();
+    return 0;
+
+}
