7992c17f5bb75b6cb068a06d5af0121480af0950
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 12:01:35 +0530

    First commit

---
+++ C/116orStatement.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int num1=22;
+int num2=24;
+
+
+    if(num1==num2 || num1%2==0){
+        printf("True");
+
+    }
+   
+    
+    else{
+        printf("False");
+    
+    }
+    return 0;
+
+}//OR statement!
