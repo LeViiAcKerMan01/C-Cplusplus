56bb4f67d3d4a3664d28a8e41fab2cc562d5e8b7
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:53:22 +0530

    First commit

---
+++ C/128pointers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int age;
+    double gpa;
+    char grade;
+    
+    printf("Age's memory address is : %p", &age);
+    return 0;
+
+}
