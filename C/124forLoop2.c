c03f9e5341c719d84732ee9232878e72d7906f0c
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:55:21 +0530

    First commit

---
+++ C/124forLoop2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int i;
+    int luckyNumbers[]={44, 8, 15, 16, 23, 42};
+    for(i=0; i<=5; i++){
+        printf("%d\n", luckyNumbers[i]);
+        
+    }
+    return 0;
+}
