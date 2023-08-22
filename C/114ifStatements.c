eba1bd69c16ec9a8bc348def3a7fc6387c1dc816
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 11:59:42 +0530

    First commit

---
+++ C/114ifStatements.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int max(int num1, int num2){
+    int result;
+    if(num1>=num2){
+        result=num1;
+    }
+    else{
+        result=num2;
+    }
+    return result;
+}
+int main()
+{
+    printf("%d", max(40, 30));
+    return 0;
+
+}
