6c9f6384870603f57086389b03311c6b4ee6db6c
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:53:09 +0530

    First commit

---
+++ C/127memoryAddress.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int age;
+    double gpa;
+    char grade;
+    
+    printf("%p", &age);
+    return 0;
+
+}// memory address accessing using pointers!
