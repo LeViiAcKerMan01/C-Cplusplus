e1d36743bac1c51df9bb51f2e197c54da9622571
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:28:10 +0530

    First commit

---
+++ C/118switchStatements.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char grade;
+    scanf("%c", &grade);
+        Switch (grade) {
+        
+        case 'A':
+        printf("You did great!");
+        break;
+        case 'B':
+        printf("You did alright!");
+        break;
+        case 'C':
+        printf("You did poorly");
+        break;
+        case 'D':
+        printf("You did very bad");
+        break;
+        case 'E':
+        printf("You failed");
+        break;
+        default :
+        printf("Invalid Grade");
+        }
+        
+        
+        return 0;
+
+}
