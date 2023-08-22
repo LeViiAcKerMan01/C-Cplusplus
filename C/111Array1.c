37ddf9efbd43aa4916958e164ba557581e5db4f4
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:55:56 +0530

    First commit

---
+++ C/111Array1.c
@@ -0,0 +1,11 @@
+// Altering elements of an array
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+    luckyNumbers[1]=29;
+    printf("%d",luckyNumbers[1]);
+    return 0;
+}
