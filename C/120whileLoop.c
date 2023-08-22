ac7a706aab8028408dc89366ed6fa0760c9dfa5b
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-22 13:28:36 +0530

    First commit

---
+++ C/120whileLoop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int index=1;
+    while(index<=5){
+        printf("%d\n", index);
+        index++;
+
+    }
+    return 0;
+}
