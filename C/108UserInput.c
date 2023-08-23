6510600b4727e136dd534a705a15cc94c7c00252
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-23 19:16:35 +0530

    First commit

---
+++ C/108UserInput.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int age;
+    printf("Enter the age :");
+    scanf("%d",&age);
+    printf("You are %d years old", age);
+}
+// scanf() is a INPUT FUNCTION!
