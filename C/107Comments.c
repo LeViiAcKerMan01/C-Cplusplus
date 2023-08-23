d785127dc999eb60654fd5e8ae972ba494de94f1
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-23 19:15:56 +0530

    First commit

---
+++ C/107Comments.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // This is a single line comment.
+    printf("Comments are fun");
+    /* 
+    This
+    is
+    a 
+    Multiline 
+    Comment
+    */
+   return 0;
+}
