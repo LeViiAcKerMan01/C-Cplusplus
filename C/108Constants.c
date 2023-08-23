9819a7a24cebf07d51d06f35e511aaa8840a9800
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-23 19:16:15 +0530

    First commit

---
+++ C/108Constants.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{   //constants can be declared by using const keyword before the variable datatype!
+    const int NUM=5; // Its a trend to initialise varible with capital letters although not necessary!
+    printf("%d\n", NUM);
+    return 0;
+}
