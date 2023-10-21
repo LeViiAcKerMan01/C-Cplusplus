d278616c9691d07173040418ac313a5ac4a35ab3
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-10-21 20:51:24 +0530

    First commit

--- Modern_C++/class_hackerrank.cpp
+++ Modern_C++/class_hackerrank.cpp
@@ -13,12 +13,34 @@ Enter code for class Student here.
 Read statement for specification.
 */
 void set_age(int aAge){
-    aAge=age;
+    age=aAge;
 }
 int get_age(){
     return age;
 }
-
+void set_first_name(string aFirst_name){
+    first_name=aFirst_name;
+}
+string get_first_name(){
+    return first_name;
+}
+void set_last_name(string aLast_name){
+    last_name=aLast_name;
+}
+string get_last_name(){
+    return last_name;
+}
+void set_standard(int aStandard){
+    standard=aStandard;
+}
+int get_standard(){
+    return standard;
+}
+string to_string(){
+    stringstream ss;
+    ss<<age<<","<<first_name<<","<<last_name<<","<<standard;
+    return ss.str();
+}
 };
 int main() {
     int age, standard;
@@ -28,6 +50,15 @@ int main() {
     
     Student st;
     st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
     cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
     return 0;
 }
