c05f7f3905ab8beb8b2489e66691bf0f55249d0f
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-11-05 19:17:51 +0530

    First commit

---
+++ stl/VECTOR_stl/102_Vector.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main()
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    
+    cout<<"Front:"<<v.front()<<endl;
+    cout<<"Back:"<<v.back()<<endl;
+
+}
